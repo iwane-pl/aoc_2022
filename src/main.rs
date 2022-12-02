b914c4a734428cc4450791b3ff0829d324407fea
iwane <46239771+iwane-pl@users.noreply.github.com>
2022-12-02 08:00:55 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 mod loader;
 mod day01;
+mod day02;
 
 #[cfg(test)]
 mod tests;
 
 use loader::*;
 use day01::*;
+use day02::*;
 
 fn main() {
-    println!("--- Advent of Code 2022 ---\n");
+    println!("--- Advent of Code 2022 ---");
 
+    println!("\n-- Day 1 --");
+    
     let contents = load(r"../../../inputs/day01.txt");
-
+    
     let calories = day01_p1(&contents);
     println!("Part 1: {calories}");
     let calories = day01_p2(&contents);
     println!("Part 2: {calories}");
     
+    println!("\n-- Day 2 --");
+    
+    let contents = load(r"../../../inputs/day02.txt");
+    
+    let score = day02_p1(&contents);
+    println!("Score 1: {score}");
+    let score = day02_p2(&contents);
+    println!("Score 2: {score}");
+
 }
--- src/tests.rs
+++ src/tests.rs
@@ -17,3 +17,20 @@ fn test_day01_p2() {
 
     assert_eq!(calories, 45000);
 }
+
+#[test]
+fn test_day02_p1() {
+    let contents = load(r"..\..\..\..\tests\day02_p1.txt");
+    
+    let points = day02_p1(&contents);
+
+    assert_eq!(points, 15);
+}
+#[test]
+fn test_day02_p2() {
+    let contents = load(r"..\..\..\..\tests\day02_p1.txt");
+    
+    let points = day02_p2(&contents);
+
+    assert_eq!(points, 12);
+}
